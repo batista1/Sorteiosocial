9a6e9b8db3d3d9d5bb9b0ff316c5ed2511704bba
Lucas Willemen <lucaswillemen@msn.com>
2016-08-12 17:57:53 -0300

    fix faq html

--- admin/controller/faq.js
+++ admin/controller/faq.js
@@ -1,25 +1,55 @@
 app.controller('faq', function($scope, $rootScope, $mdDialog){
 	$scope.card = []
-	$scope.bnt_new = function(ev) {
+    $scope.bnt_new = function(ev) {
         $mdDialog.show({
             templateUrl: 'modal/faq.html?' + window.version,
             targetEvent: ev,
             ariaLabel: 'Publicação',
             clickOutsideToClose: true,
             controller: function($scope, $localStorage, $rootScope, $mdDialog) {
-            	$scope.save = function(){
-            		console.log($scope.data)
-            		$rootScope.socket.emit("faq_save", $scope.data)
-            		$scope.cancel = function(){ $mdDialog.cancel()}
-            	}   
+                $scope.save = function(){
+                    console.log($scope.data)
+                    $rootScope.socket.emit("faq_save", $scope.data)
+                    $scope.cancel = function(){ $mdDialog.cancel()}
+                }   
             }
         })
-    }	            
-	$rootScope.socket.on("faq_save", function(res){
-		$mdDialog.cancel()
-		console.log(res)
-		$scope.card.push(res)
-	})
+    }    
+    $scope.bnt_edit = function(data, ev) {
+        $rootScope.data = data
+        console.log(data)
+        $mdDialog.show({
+            templateUrl: 'modal/faq.html?' + window.version,
+            targetEvent: ev,
+            ariaLabel: 'Publicação',
+            clickOutsideToClose: true,
+            controller: function($scope, $localStorage, $rootScope, $mdDialog) {
+                $scope.data = $rootScope.data
+                $scope.save = function(){
+                    console.log($scope.data)
+                    $rootScope.socket.emit("faq_edit", $scope.data)
+                    $scope.cancel = function(){ $mdDialog.cancel()}
+                }   
+            }
+        })
+    }               
+    $rootScope.socket.on("faq_save", function(res){
+        $mdDialog.cancel()
+        console.log(res)
+        $scope.card.push(res)
+    })         
+
+    
+    $rootScope.socket.on("faq_edit", function(res){
+        $mdDialog.cancel()
+        angular.forEach($scope.card, function(v, k){
+            if (res._id == v._id) {
+                $scope.card[k]=res
+                $scope.$apply()
+            }
+
+        })
+    })
     $rootScope.socket.emit("faq_load")
     $rootScope.socket.removeListener("faq_load")
     $rootScope.socket.on("faq_load", function(res){
