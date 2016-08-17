afc439558d3ef6f20aa96210cf1d1841465349a0
Lucas Willemen <lucaswillemen@msn.com>
2016-08-17 14:49:10 -0300

    login debug

--- controller/autologin.js
+++ controller/autologin.js
@@ -1,5 +1,6 @@
 app.controller("autologin", function($scope, $rootScope, $state, $localStorage){
 	var sk = $rootScope.socket
+	console.log(localStorage.login)
 	sk.emit("autologin", localStorage.login)
 	sk.on("autologin", function(res){
 		console.log(res)
